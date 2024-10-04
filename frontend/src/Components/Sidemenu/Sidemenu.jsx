d7b5e1dd58ebfc9460e818bd7fc32f5bdee3b312
babygrandad <relebogilenkotswe@gmail.com>
2024-10-04 17:01:25 +0200

    front end changes

---
+++ frontend/src/Components/Sidemenu/Sidemenu.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './Sidemenu.module.css'
+import logo from '../../assets/Garden Palette Logo.png'
+import {Store, Person, ShoppingCart, ManageAccounts, Logout } from '@mui/icons-material';
+function Sidemenu() {
+	return (
+		<aside className={`${styles['sidemenu-container']}`}>
+			<button className={styles["menu-button"]} data-role="toggle">
+        O
+      </button>
+      <div className={styles["sidemenu-wrapper"]}>
+        <div className={styles["logo-container"]}>
+          <img src={logo} alt="" className={styles["logo-image"]} />
+        </div>
+        <nav>
+          <ul>
+            <li><a href=""><Store /><span>Shop</span></a></li>
+            <li><a href=""><Person /><span>Profile</span></a></li>
+            <li><a href=""><ShoppingCart /><span>Cart</span></a></li>
+            <li><a href=""><ManageAccounts /><span>Management</span></a></li>
+          </ul>
+        </nav>
+        <div className={styles["user-profile"]}>
+          <img src="https://i.pinimg.com/736x/e4/ea/7a/e4ea7a052f09288f1490c1037b3f7181.jpg" alt="" className={styles["user-profile-image"]} />
+          <span className={styles["user-profile-name"]}>Naruto Uzumaki</span>
+          <span className={styles["user-profile-email"]}>naruto@example.com</span>
+        </div>
+        <div className={styles["logout-container"]}>
+          <ul><li><a href=""><Logout /><span>Logout</span></a></li></ul>
+        </div>
+      </div>
+		</aside>
+	)
+}
+
+export default Sidemenu
+

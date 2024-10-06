43e0a4a549b05b7d82961b558a6020ce91893fc5
babygrandad <relebogilenkotswe@gmail.com>
2024-10-06 17:00:59 +0200

    placed reminder on code to work on filter

--- frontend/src/Components/Topbar/Topbar.jsx
+++ frontend/src/Components/Topbar/Topbar.jsx
@@ -35,7 +35,7 @@ function Topbar({all = 0, fruit = 0, veg = 0, onFilterChange, onSearch}) {
       <button className={styles['header-menu-toggle']}><Menu /></button>
       <form className={styles["header-search-form"]} autoComplete='off' onSubmit={handleSubmit}>
         <Search />
-        <input type="text" value={searchInput} className={styles['search-input']} placeholder={`Search...`} onChange={handleInputChange} />
+        <input type="text" value={searchInput} className={styles['search-input']} placeholder={`! ! ! work on the filter ! ! !`} onChange={handleInputChange} />
       </form>
       <div className={styles["user-profile"]}>
           <img src="https://i.pinimg.com/736x/e4/ea/7a/e4ea7a052f09288f1490c1037b3f7181.jpg" alt="" className={styles["user-profile-image"]} />

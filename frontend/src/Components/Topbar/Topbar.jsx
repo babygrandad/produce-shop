a9c46565a51f6f3be008be2d19dc65ae91c19e47
babygrandad <relebogilenkotswe@gmail.com>
2024-10-06 16:46:33 +0200

    need to update search funtion now

--- frontend/src/Components/Topbar/Topbar.jsx
+++ frontend/src/Components/Topbar/Topbar.jsx
@@ -33,9 +33,9 @@ function Topbar({all = 0, fruit = 0, veg = 0, onFilterChange, onSearch}) {
   return(
     <header className={styles['header-container']}>
       <button className={styles['header-menu-toggle']}><Menu /></button>
-      <form className={styles["header-search-form"]} autoComplete='off'>
+      <form className={styles["header-search-form"]} autoComplete='off' onSubmit={handleSubmit}>
         <Search />
-        <input type="text" className={styles['search-input']} placeholder={`Search...`} />
+        <input type="text" value={searchInput} className={styles['search-input']} placeholder={`Search...`} onChange={handleInputChange} />
       </form>
       <div className={styles["user-profile"]}>
           <img src="https://i.pinimg.com/736x/e4/ea/7a/e4ea7a052f09288f1490c1037b3f7181.jpg" alt="" className={styles["user-profile-image"]} />
